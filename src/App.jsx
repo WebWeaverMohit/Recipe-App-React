e4322f3791a1235857d9e7d64cf289eb164d9ce9
WebWeaverMohit <mohitshrivastava0000@gmail.com>
2024-05-15 11:36:55 +0530

    Recipe App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from './components/Nav'
+import Home from './components/Home'
 
 const App = () => {
   return (
     <div>
       <div className='w-full h-screen bg-black py-2 px-8'>
         <Nav />
+        <Home />
       </div>
     </div>
   )
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='h-[75%] mt-10  w-full bg-red-500 '>
+            <img className='object-cover object-bottom h-full w-full' src="https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        </div>
+    )
+}
+
+export default Home
